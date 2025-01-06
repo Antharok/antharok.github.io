a99516c8a23480781e3273d9addd87f70551c289
Antharok <81885133+Antharok@users.noreply.github.com>
2025-01-06 04:34:58 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Simple JavaScript to handle button click
-document.getElementById('clickButton').addEventListener('click', function() {
-  document.getElementById('message').innerText = "You clicked the button!";
-});
