16804fe6d0f5c16e2934d92ac029804c9643d3f5
Antharok <81885133+Antharok@users.noreply.github.com>
2025-01-06 04:29:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple JavaScript to handle button click
+document.getElementById('clickButton').addEventListener('click', function() {
+  document.getElementById('message').innerText = "You clicked the button!";
+});
